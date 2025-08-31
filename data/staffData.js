2b286f490b525ad675074a578e154189df092383
SajithTilanga <sajiththilanga94@gmail.com>
2025-08-31 22:12:28 +0530

    Add files via upload
    change name simple capital

---
+++ data/staffData.js
@@ -0,0 +1,436 @@
+// data/staffData.js
+import secImg from '../assets/sampath.jpeg';
+import nalika from '../assets/nalika.jpeg';
+import radha from '../assets/radha.png';
+import hema from '../assets/hema.jpeg';
+import sisira from '../assets/sisira.jpeg';
+import kumara from '../assets/kumara.jpeg';
+import lakmini from '../assets/lakmini.jpeg';
+import lanika from '../assets/lanika.jpeg';
+import nilantha from '../assets/nilantha.jpeg';
+import nilanthi from '../assets/nilanthi.jpeg';
+import pradeep from '../assets/pradeep.jpeg';
+import sadaruwan from '../assets/sadaruwan.jpeg';
+import ac from '../assets/ac.jpeg';
+import uthpala from '../assets/uthpala.jpeg';
+import chandana from '../assets/chandana.jpeg';
+import chandani from '../assets/chandani.jpeg';
+import danthika from '../assets/danthika.jpeg';
+import dilupa from '../assets/dilupa.jpeg';
+import dinari from '../assets/dinari.jpeg';
+import gayani from '../assets/gayani.jpeg';
+import yoga from '../assets/yoga.png';
+import wasana from '../assets/wasana.png';
+import milanthi from '../assets/milanthi.jpeg';
+import jeewanthi from '../assets/jeewanthi.png';
+
+
+export const staffData = {
+  secretary: {
+    id: "sec-001",
+    name: "Sampath Mantrinayake",
+    position: "Secretary - SLAS (SPG)",
+    department: "Ministry Office",
+    education: "Msc Law",
+    phone: "+94 11 234 5670",
+    email: "sampath@ministry.gov.lk",
+    joiningDate: "2020-01-15",
+    image: secImg
+  },
+  additionalSecretaries: [
+    {
+      id: "addsec-001",
+      name: "Radha Nanayakkara",
+      position: "Additional Secretary - SLAS (SPG)",
+      department: "Admin",
+      education: "MA Sociology",
+      phone: "071-51347448",
+      email: "radhasrimathi@yahoo.com",
+      joiningDate: "2020-01-15",
+      image: radha
+    },
+    {
+      id: "addsec-002",
+      name: "Nalika Piyasena",
+      position: "Additional Secretary - SLAS (SPG)",
+      department: "Rural Development Division",
+      education: "-",
+      phone: "071-8009440",
+      email: "htrnpiyasena@gmail.com",
+      joiningDate: "2020-01-15",
+      image: nalika
+    },
+    {
+      id: "addsec-003",
+      name: "Hema Perera",
+      position: "Additional Secretary - SLAS (SPG)",
+      department: "Social Security Division",
+      education: "MA in Social Policy",
+      phone: "+94 11 234 5682",
+      email: "hema@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: hema
+    },
+    {
+      id: "addsec-004",
+      name: "Gamini Wijesinghe",
+      position: "Additional Secretary - SLAS (SPG)",
+      department: "Community Empowerment Division",
+      education: "MSc in Community Development",
+      phone: "+94 11 234 5683",
+      email: "gamini@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=150&h=150&fit=crop&crop=face"
+    }
+  ],
+  cfo: {
+    id: "cfo-001",
+    name: "Lakmini",
+    position: "Chief Financial Officer",
+    department: "Finance Division",
+    education: "MBA Finance, ACCA",
+    phone: "+94 11 234 5672",
+    email: "lakmini@ministry.gov.lk",
+    joiningDate: "2020-01-15",
+    image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=150&h=150&fit=crop&crop=face"
+  },
+  directorGenerals: [
+    {
+      id: "dg-001",
+      name: "Sisira Kumara",
+      position: "Director General - SLAS (SPG)",
+      department: "Rural Development Bureau",
+      education: "MSc in Management",
+      phone: "+94 11 234 5673",
+      email: "sisira@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: sisira
+    },
+    {
+      id: "dg-002",
+      name: "Mr. Planning",
+      position: "Director General - Administration",
+      department: "Administration Division",
+      education: "MBA in Public Administration",
+      phone: "+94 11 234 5684",
+      email: "planning@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: "https://images.unsplash.com/photo-1551836022-deb4988cc6c0?w=150&h=150&fit=crop&crop=face"
+    }
+  ],
+  directors: [
+    {
+      id: "dir-001",
+      name: "Lanika Jayakodi",
+      position: "Director",
+      department: "Planning Division",
+      education: "MSc in Rural Development",
+      phone: "+94 11 234 5674",
+      email: "kavitha@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: lanika
+    },
+    {
+      id: "dir-002",
+      name: "Nilantha Wickramanayake",
+      position: "Director - SLPS II",
+      department: "Rural Development Bureau",
+      education: "M.Sc(Aquatic Bio Resources Management and Aquaculture) - Post Graduate Institute of Agriculture, University of Peradeniya",
+      phone: "071-8318434",
+      email: "nilanthawickramanayake@gmail.com",
+      joiningDate: "2020-01-15",
+      image: nilantha
+    },
+    {
+      id: "dir-003",
+      name: "Pradeep Wickramage",
+      position: "Director - SLPS II",
+      department: "Rural Development Bureau",
+      education: "Master of Regional Development & Planning - University of Colombo",
+      phone: "071-8025137",
+      email: "suranga.wick75@gmail.com",
+      joiningDate: "4 years & 9 months",
+      image: pradeep
+    },
+    {
+      id: "dir-004",
+      name: "Uthpala Wijekoon",
+      position: "Director - SLPS II",
+      department: "Rural Development Bureau",
+      education: "MA(International Relations) - International Christian University, Tokyo, Japan",
+      phone: "071-4434091",
+      email: "uthlk@yahoo.com",
+      joiningDate: "2020-01-15",
+      image: uthpala
+    },
+    {
+      id: "dir-005",
+      name: "Nilanthi Perera",
+      position: "Director - Community Empowerment",
+      department: "World Food Program",
+      education: "MSc in Community Development",
+      phone: "+94 11 234 5686",
+      email: "dilani@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: nilanthi
+    },
+    {
+      id: "dir-006",
+      name: "Dinari Ariyarathna",
+      position: "Director - SLPS-II",
+      department: "World Food Programme",
+      education: "Master of Public Administration & Management/ Master of International Cooperation",
+      phone: "+94 11 234 5687",
+      email: "sampathf@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: dinari
+    },
+    {
+      id: "dir-007",
+      name: "Wasana Gunarathna",
+      position: "Director SLAS-II-I",
+      department: "Community Empowerment",
+      education: "MA(Sociology) / LLB(Law) - Open University",
+      phone: "071-2793270",
+      email: "wasgun1977@gmail.com",
+      joiningDate: "2020-01-15",
+      image: wasana
+    },
+    {
+      id: "dir-008",
+      name: "Danthika",
+      position: "Director - Legal Affairs",
+      department: "Legal Division",
+      education: "LLB, Attorney-at-Law",
+      phone: "+94 11 234 5689",
+      email: "buddhika@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: danthika
+    },
+    {
+      id: "dir-009",
+      name: "G. D. D. Kumara",
+      position: "Director - SLPS II-I",
+      department: "Rural Development",
+      education: "Master of Regional development & Planning - University of Colombo",
+      phone: "071-1270705",
+      email: "gddkumara@yahoo.com",
+      joiningDate: "2020-01-15",
+      image: kumara
+    },
+    {
+      id: "dir-010",
+      name: "P. J. D. Fernando",
+      position: "Senior Assistant Secretary - SLAS I",
+      department: "Admin",
+      education: "Master of Public Management - Sri Lanka Institute of Development Administration",
+      phone: "077-9886972",
+      email: "jeewanthiefdo@yahoo.com",
+      joiningDate: "2020-01-15",
+      image: jeewanthi
+    },
+    {
+      id: "dir-011",
+      name: "Ms. Chandrika Bandara",
+      position: "Director - Public Relations",
+      department: "Public Relations",
+      education: "MA in Mass Communication",
+      phone: "+94 11 234 5692",
+      email: "chandrika@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: "https://images.unsplash.com/photo-1551836022-deb4988cc6c0?w=150&h=150&fit=crop&crop=face"
+    },
+    {
+      id: "dir-012",
+      name: "Mr. Gayan Jayasuriya",
+      position: "Director - Procurement",
+      department: "Procurement Division",
+      education: "MSc in Supply Chain Management",
+      phone: "+94 11 234 5693",
+      email: "gayan@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=150&h=150&fit=crop&crop=face"
+    }
+  ],
+  chiefAccountant: {
+    id: "ca-001",
+    name: "G. N. Lakmini",
+    position: "Chief Accountant - SLAcS-I",
+    department: "Accounting Division",
+    education: "MA(ECON)",
+    phone: "+94 11 234 5694",
+    email: "malini@ministry.gov.lk",
+    joiningDate: "5 months",
+    image: lakmini
+  },
+  chiefInternalAuditor: {
+    id: "cia-001",
+    name: "Mr. Roshan Fernando",
+    position: "Chief Internal Auditor",
+    department: "Internal Audit Division",
+    education: "ACCA, MSc in Auditing",
+    phone: "+94 11 234 5695",
+    email: "roshan@ministry.gov.lk",
+    joiningDate: "2020-01-15",
+    image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=150&h=150&fit=crop&crop=face"
+  },
+  deputyDirectors: [
+    {
+      id: "dd-001",
+      name: "Chandani Ramasinghe",
+      position: "Deputy Director - SLPS II",
+      department: "Rural Development Bureau",
+      education: "MA(Development Studies) - Eramus University, The Netherlands",
+      phone: "071-5344071",
+      email: "ramasinghe@gmail.com",
+      joiningDate: "5 years",
+      image: chandani
+    },
+    {
+      id: "dd-002",
+      name: "Chandana Wijewardhana",
+      position: "Deputy Director - SLPS II",
+      department: "Rural Development Bureau",
+      education: "MA(Sociology) - University of Kelaniya ",
+      phone: "070-2599805",
+      email: "wijewardhana87@gmail.com",
+      joiningDate: "2020-01-15",
+      image: chandana
+    },
+    {
+      id: "dd-003",
+      name: "Gayani Anuruddika",
+      position: "Assistant Director - SLPS III",
+      department: "World Food Program",
+      education: "Masters in Economics - University of Kelaniya / MAAT",
+      phone: "+94 11 234 5696",
+      email: "upul@ministry.gov.lk",
+      joiningDate: "6 months",
+      image: gayani
+    },
+    {
+      id: "dd-004",
+      name: "M.M. Gamage",
+      position: "Deputy Director SLPS-II",
+      department: "Social Security",
+      education: "B.Sc. Degree(Bio Science) - University of Kelaniya",
+      phone: "+94 11 234 5697",
+      email: "kumudini@ministry.gov.lk",
+      joiningDate: "10 years, 04 months",
+      image: milanthi
+    },
+    {
+      id: "dd-005",
+      name: " Chanaka Sandaruwan Rathnayake",
+      position: "Assistant Director - SLAS III",
+      department: "Rural Development Bureau",
+      education: "BA(Business Statistics)- University of Sri Jayawardanapura",
+      phone: "070-3065277",
+      email: "rmcsrathnayaka00@gmail.com",
+      joiningDate: "2020-01-15",
+      image: sadaruwan
+    },
+    {
+      id: "dd-006",
+      name: "K Yohanathan",
+      position: "Assistant Director - SLPS II-I",
+      department: "Rural Development",
+      education: "Master of Sociology - Madurai Kamarajar University, India",
+      phone: "077-7284657",
+      email: "yohanathan@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: yoga
+    },
+    {
+      id: "dd-007",
+      name: "Kumara Mendis",
+      position: "Assistant Director - SLPS-II",
+      department: "Social Security",
+      education: "BA/PGDIR-BCIS/PGDSNLDP/MRDP - Colombo University",
+      phone: "+94 11 234 5700",
+      email: "chathura@ministry.gov.lk",
+      joiningDate: "10 years 7 months",
+      image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=150&h-150&fit=crop&crop=face"
+    },
+    {
+      id: "dd-008",
+      name: "Ms. Amali Wickremasinghe",
+      position: "Deputy Director - Legal Services",
+      department: "Legal Division",
+      education: "LLB, Attorney-at-Law",
+      phone: "+94 11 234 5701",
+      email: "amali@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?w=150&h-150&fit=crop&crop=face"
+    },
+    {
+      id: "dd-009",
+      name: "Mr. Sisira Fernando",
+      position: "Additional Director - Project Management",
+      department: "Planning Division",
+      education: "MSc in Project Management",
+      phone: "+94 11 234 5702",
+      email: "sisiraf@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: "https://images.unsplash.com/photo-1556157382-97eda2d62296?w=150&h-150&fit=crop&crop=face"
+    },
+    {
+      id: "dd-010",
+      name: "Ms. Ruwani Dias",
+      position: "Deputy Director - Public Relations",
+      department: "Public Relations",
+      education: "MA in Communication",
+      phone: "+94 11 234 5703",
+      email: "ruwani@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=150&h-150&fit=crop&crop=face"
+    }
+  ],
+  accountants: [
+    {
+      id: "acc-001",
+      name: "S.A.R.R.Sooriyaarachchi",
+      position: "Accountant - SLAcS III",
+      department: "Accounts Division",
+      education: "CIMA UK membership (ACMA)/ Masters in Business Administration J'pura",
+      phone: "+94 11 234 5681",
+      email: "dinesh@ministry.gov.lk",
+      joiningDate: "1 year and 1 month",
+      image: ac
+    },
+    {
+      id: "acc-002",
+      name: "Ms. Priyanka Wijesinghe",
+      position: "Accountant",
+      department: "Accounts Division",
+      education: "BSc in Accounting, CIMA",
+      phone: "+94 11 234 5704",
+      email: "priyanka@ministry.gov.lk",
+      joiningDate: "2020-01-15",
+      image: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?w=150&h-150&fit=crop&crop=face"
+    }
+  ]
+};
+
+// Helper function to get all staff as a flat array
+export const getAllStaff = () => {
+  return [
+    staffData.secretary,
+    ...staffData.additionalSecretaries,
+    staffData.cfo,
+    ...staffData.directorGenerals,
+    ...staffData.directors,
+    staffData.chiefAccountant,
+    staffData.chiefInternalAuditor,
+    ...staffData.deputyDirectors,
+    ...staffData.accountants
+  ];
+};
+
+// Helper function to find staff by name slug
+export const findStaffByNameSlug = (nameSlug) => {
+  const allStaff = getAllStaff();
+  return allStaff.find(person => 
+    person.name.toLowerCase().replace(/\s+/g, '-') === nameSlug
+  );
+};
